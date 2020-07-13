4daaed0ab343deb760829ac113dd9bc1b33e4b99
Mario <mario.martins@sonda.com>
2020-07-13 18:05:09 -0300

    3.13. Exercício 05: Constantes

--- src/br/algaworks/logica/capitulo03/Exercicio_3.java
+++ src/br/algaworks/logica/capitulo03/Exercicio_3.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 
 public class Exercicio_3 {
 
+	private static final Integer MEDIA_PARA_APROVACAO = 70;
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Entre com a nota: ");
 		Double nota = scanner.nextDouble();
 
-		Boolean resultado = nota >= 70;
+		Boolean resultado = nota >= MEDIA_PARA_APROVACAO;
 
 		if (resultado) {
 			System.out.print("O aluno foi aprovado !");
