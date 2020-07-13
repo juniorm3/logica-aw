ce876d6e3ad69b12d852110801095131db682053
Mario <mario.martins@sonda.com>
2020-07-13 17:33:29 -0300

    3.11. Exercício 04: Encontre o valor total baseado na quantidade e no desconto

---
+++ src/br/algaworks/logica/capitulo03/Exercicio04.java
@@ -0,0 +1,30 @@
+package br.algaworks.logica.capitulo03;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Informe o valor do produto: ");
+		double valorProduto = scanner.nextDouble();
+
+		System.out.print("Informe a quantidade do produto: ");
+		int quantidadeProduto = scanner.nextInt();
+
+		double subTotal = valorProduto * quantidadeProduto;
+
+		double desconto = 0.0;
+
+		if (quantidadeProduto > 10) {
+			desconto = subTotal * 10 / 100;
+		}
+		
+		double totalFinal = subTotal - desconto;
+		
+		System.out.println("O valor final do pedido e: R$" + totalFinal);
+		
+		scanner.close();
+	}
+
+}
