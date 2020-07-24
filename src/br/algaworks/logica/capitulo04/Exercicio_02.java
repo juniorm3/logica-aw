84b9759f1d79da1d35bec5dd88770f6ebf42cfb8
Mario Junior <juniorm3@msn.com>
2020-07-23 21:00:59 -0300

    6.5. Exercício 02: Repita o exercício anterior com o while

---
+++ src/br/algaworks/logica/capitulo04/Exercicio_02.java
@@ -0,0 +1,22 @@
+package br.algaworks.logica.capitulo04;
+
+public class Exercicio_02 {
+
+	public static void main(String[] args) {
+		Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+		int resultado = 0;
+		int indice = 0;
+		while (indice < numeros.length) {
+			resultado = numeros[indice] % 3;
+			if (resultado != 0) {				
+				indice++;
+			} else {
+				System.out.println(numeros[indice]);				
+			}
+			indice++;
+		}
+
+	}
+
+}
