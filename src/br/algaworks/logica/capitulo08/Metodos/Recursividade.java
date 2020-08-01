66317eb1652e27ff5e2f17bcd6ba0aaf943678fc
Mario Junior <juniorm3@msn.com>
2020-07-31 21:06:20 -0300

    8.8. Recursividade

---
+++ src/br/algaworks/logica/capitulo08/Metodos/Recursividade.java
@@ -0,0 +1,27 @@
+package br.algaworks.logica.capitulo08.Metodos;
+
+public class Recursividade {
+
+	public static void main(String[] args) {
+
+//		String[] alunos = new String[] {"Alexandre", "João", "Maria" };
+//		iterarNomes(alunos, 0);
+		
+	}
+
+//	static void iterarNomes(String[] alunos, Integer i) {
+//		System.out.println("Aluno: " + alunos[i]);
+//		
+//		if(++i < alunos.length) {
+//			iterarNomes(alunos, i);
+//		}
+//	}
+	
+//	static void imprimirNumero(Integer numero) {
+//		System.out.println("Numero: " + numero);
+//
+//		if (numero < 10) {
+//			imprimirNumero(++numero);
+//		}
+//	}
+}
