af905b60b974100f2e8b6defd65a84bb39aeb46e
Mario Jr <juniorm3@msn.com>
2020-09-24 23:02:16 -0300

    10.3. Exercício 01: Crie uma lista de tarefas e salve a lista em um arquivo

---
+++ src/br/algaworks/logica/capitulo10/escritaarquivos/Exercicio.java
@@ -0,0 +1,30 @@
+package br.algaworks.logica.capitulo10.escritaarquivos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+
+		ArrayList<String> atividades = new ArrayList<String>();
+		String atividade = "";
+
+		while (!atividade.equals("x")) {
+			System.out.print("Informar atividade: ");
+			atividade = scanner.nextLine();
+			atividades.add(atividade);
+		}
+		
+		System.out.println("Fim...");
+
+		Path arquivo = Paths.get("D:\\ESTUDO\\tmp\\aula\\exercicio.txt");
+		Files.write(arquivo, atividades);
+	}
+
+}
