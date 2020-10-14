d70752db984d092fe8db5e7394c967e7d99e58c9
Mario <juniorm#@msn.com>
2020-10-13 21:58:03 -0300

    11.5. Criando o sua própria biblioteca

---
+++ src/br/algaworks/logica/capitulo11/blibiotecapropia/BibliotecaPropia.java
@@ -0,0 +1,23 @@
+package br.algaworks.logica.capitulo11.blibiotecapropia;
+
+import br.algaworks.logica.capitulo11.algaworksconsole.Interacao;
+
+
+public class BibliotecaPropia {
+
+	public static void main(String[] args) {
+//		Scanner scannner = new Scanner(System.in);
+		
+		Interacao interacao = new Interacao();
+		
+		Integer numero = interacao.lerNumero("Digite um número: ");
+		interacao.imprimir("Número: " + numero);
+		
+		Double decimal = interacao.lerDecimal("Digite um número decimal: ");
+		interacao.imprimir("Decimal: " + decimal);
+		
+		interacao.erro("Essa é uma mensagem de erro!");
+		
+		interacao.fechar();
+	}
+}
